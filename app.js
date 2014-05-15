916c4c13d74b22a36f6add59b1e982094cd9714b
Sarah Groff-Palermo <sarah.groff.palermo@gmail.com>
2014-05-15 12:44:43 -0700

    Circle drawn, colored based on location

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ console.log('rawr');
 var you = document.getElementById('you');
 var robot = document.getElementById('robot');
 
+var marginX = 10, // Equal to margin on body
+    marginY = 60; // Equal to margin on body, plus score div height
+
 function logCo (event){
   console.log(event.clientX, event.clientY);
 };
 
-you.onclick = logCo;
-robot.onclick = logCo;
+function drawCircle (event) {
+  var x = event.clientX - marginX,
+      y = event.clientY - marginY,
+      hue = x%360;
+
+
+  you.innerHTML = '<circle cx="' + x + '" cy="' + y + '" r="10" style="fill:hsla('+ hue + ', 100%, 50%, 1)"/>'
+}
+
+you.onmouseup = logCo;
+robot.onmouseup = logCo;
+
+you.onmousedown = drawCircle;
