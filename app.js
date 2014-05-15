1deec7afb97244583e67e50248a3c29ce2c85235
Sarah Groff-Palermo <sarah.groff.palermo@gmail.com>
2014-05-15 11:53:26 -0700

    Mouse location logging on click

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("rawr");
+console.log('rawr');
+
+var you = document.getElementById('you');
+var robot = document.getElementById('robot');
+
+function logCo (event){
+  console.log(event.clientX, event.clientY);
+};
+
+you.onclick = logCo;
+robot.onclick = logCo;
