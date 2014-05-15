34c45230246b40fc51138d76b100d83d0e1d2823
Sarah Groff-Palermo <sarah.groff.palermo@gmail.com>
2014-05-15 15:13:36 -0700

    Circles appearing on both; lightness domain/range set, getting ready for help class hover

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ console.log('rawr');
 var you = document.getElementById('you');
 var robot = document.getElementById('robot');
 
+var width = robot.offsetWidth,
+    height = robot.offsetHeight;
+
 var marginX = 10, // Equal to margin on body
     marginY = 60; // Equal to margin on body, plus score div height
 
@@ -10,16 +13,61 @@ function logCo (event){
   console.log(event.clientX, event.clientY);
 };
 
+function lightnessRange (num, height, usefulMin, usefulMax) {
+  var usefulMin = usefulMin || 20,
+      usefulMax = usefulMax || 85,
+      difference = usefulMax - usefulMin;
+
+  return (num - marginY) / height * difference + usefulMin;
+}
+
 function drawCircle (event) {
   var x = event.clientX - marginX,
       y = event.clientY - marginY,
-      hue = x%360;
+      hue = x%360,
+      lightness = lightnessRange(y, height);
+
+  var inner;
+    inner = '<circle cx="';
+    inner += x;
+    inner += '" cy="'; 
+    inner += y; 
+    inner += '" r="10" style="fill:hsla(';
+    inner += hue; 
+    inner += ', 100%,';  
+    inner += lightness; 
+    inner += '%, 1)"/>';
+
+  you.innerHTML = inner;
+};
+
+function anyNum (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 
-  you.innerHTML = '<circle cx="' + x + '" cy="' + y + '" r="10" style="fill:hsla('+ hue + ', 100%, 50%, 1)"/>'
-}
+function roboCircle() {
+  var x = anyNum(0, width) - marginX,
+      y = anyNum(50, 500) - marginY,
+      hue = x%360,
+      lightness = lightnessRange(y, height);
+
+  var roboCirc;
+    roboCirc = '<circle cx="';
+    roboCirc += x;
+    roboCirc += '" cy="'; 
+    roboCirc += y; 
+    roboCirc += '" r="10" style="fill:hsla(';
+    roboCirc += hue;
+    roboCirc += ', 100%, ';  
+    roboCirc += lightness; 
+    roboCirc += '%, 1)"/>';
+
+  robot.innerHTML = roboCirc;
+};
 
 you.onmouseup = logCo;
 robot.onmouseup = logCo;
 
 you.onmousedown = drawCircle;
+robot.onmousedown = roboCircle;
